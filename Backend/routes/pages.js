422f21e6b55ade6f66da578c406c06d4e9f6712e
prateekch33 <prateekchandra27@outlook.com>
2023-01-14 20:30:19 +0530

    Added FaQ Pages

--- Backend/routes/pages.js
+++ Backend/routes/pages.js
@@ -19,6 +19,7 @@ page.get("/:page", (req, res) => {
   else if (page == "gallery") res.render("gallery");
   else if (page == "aDashboard") res.render("admin-panel");
   else if (page == "dashboard") res.render("student-page");
+  else if (page=="faq") res.render("faq");
   else if (page == "resume") res.render("resume-builder", { check: false });
 });
 
