82f286ab1ee1515ce67ce24fb2f321be7d3ded6d
Prem Kumar <premraj259@outlook.com>
2023-05-23 17:01:25 +0530

    added thank you page and fixed footer links along with nav css

--- Backend/routes/pages.mjs
+++ Backend/routes/pages.mjs
@@ -21,6 +21,7 @@ page.get("/:page", (req, res) => {
   else if (page == "dashboard") res.render("student-page");
   else if (page == "faq") res.render("faq");
   else if (page == "jaf") res.render("jaf");
+  else if (page == "thank") res.render("thank");
   else if (page == "resume") res.render("resume-builder", { check: false });
 });
 
