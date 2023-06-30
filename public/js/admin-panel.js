2295b32c25c2f0ae7deef7d775d11490b15f3ca2
prateekch33 <prateekchandra27@outlook.com>
2023-06-30 18:53:31 +0530

    TPO Data Updated

--- public/js/admin-panel.js
+++ public/js/admin-panel.js
@@ -3,144 +3,144 @@
 
 if (window.innerWidth < 992) {
   window.location.href = "/notview";
-} else {
-  var newsHistory = document.querySelector("#news-history");
-  var eventsHistory = document.querySelector("#events-history");
-  var newsAdd = document.querySelector("#news-add");
-  var eventsAdd = document.querySelector("#events-add");
-  var newsAddBtn = document.querySelector("#news-ab");
-  var eventsAddBtn = document.querySelector("#events-ab");
-  var newsImg = document.querySelector("#news-img");
-  var eventsImg = document.querySelector("#events-img");
-  var backBtn = document.querySelector("#b-btn");
-  var imgUpd = document.querySelector("#img-upd");
-  var imgShow = document.querySelector("#img-show");
-  var detailsShow = document.querySelector("#show-details");
-  var recruiterDetails = document.querySelector(".recruiters-details");
+}
+var newsHistory = document.querySelector("#news-history");
+var eventsHistory = document.querySelector("#events-history");
+var newsAdd = document.querySelector("#news-add");
+var eventsAdd = document.querySelector("#events-add");
+var newsAddBtn = document.querySelector("#news-ab");
+var eventsAddBtn = document.querySelector("#events-ab");
+var newsImg = document.querySelector("#news-img");
+var eventsImg = document.querySelector("#events-img");
+var backBtn = document.querySelector("#b-btn");
+var imgUpd = document.querySelector("#img-upd");
+var imgShow = document.querySelector("#img-show");
+var detailsShow = document.querySelector("#show-details");
+var recruiterDetails = document.querySelector(".recruiters-details");
 
+eventsAdd.style.display = "none";
+
+newsAddBtn.addEventListener("click", () => {
+  newsImg.src = "./assets/option-icon-active.svg";
+  eventsImg.src = "./assets/option-icon-inactive.svg";
+  newsAdd.style.display = "block";
   eventsAdd.style.display = "none";
+});
 
-  newsAddBtn.addEventListener("click", () => {
-    newsImg.src = "./assets/option-icon-active.svg";
-    eventsImg.src = "./assets/option-icon-inactive.svg";
-    newsAdd.style.display = "block";
-    eventsAdd.style.display = "none";
-  });
+eventsAddBtn.addEventListener("click", () => {
+  eventsImg.src = "./assets/option-icon-active.svg";
+  newsImg.src = "./assets/option-icon-inactive.svg";
+  eventsAdd.style.display = "block";
+  newsAdd.style.display = "none";
+});
 
-  eventsAddBtn.addEventListener("click", () => {
-    eventsImg.src = "./assets/option-icon-active.svg";
-    newsImg.src = "./assets/option-icon-inactive.svg";
-    eventsAdd.style.display = "block";
-    newsAdd.style.display = "none";
-  });
+imgUpd.addEventListener("change", (e) => {
+  imgShow.src = URL.createObjectURL(e.target.files[0]);
+});
+imgShow.addEventListener("load", () => {
+  URL.revokeObjectURL(imgShow.src);
+});
 
-  imgUpd.addEventListener("change", (e) => {
-    imgShow.src = URL.createObjectURL(e.target.files[0]);
-  });
-  imgShow.addEventListener("load", () => {
-    URL.revokeObjectURL(imgShow.src);
-  });
-
-  document.forms["news-add"].addEventListener("submit", (e) => {
-    e.preventDefault();
-    fetch(`/api/news/newsUpload`, {
-      method: "POST",
-      headers: {
-        auth_token: localStorage.getItem("adminToken"),
-      },
-      body: new URLSearchParams(new FormData(e.target)),
+document.forms["news-add"].addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(`/api/news/newsUpload`, {
+    method: "POST",
+    headers: {
+      auth_token: localStorage.getItem("adminToken"),
+    },
+    body: new URLSearchParams(new FormData(e.target)),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.status == 0) {
+        alert("News Added Successfully!!");
+        window.location.reload();
+        // fetchNewsData();
+        // fetchEventsData();
+        // historyItems.innerHTML = html;
+      } else {
+        alert("Something went wrong while adding News!!");
+      }
     })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.status == 0) {
-          alert("News Added Successfully!!");
-          window.location.reload();
-          // fetchNewsData();
-          // fetchEventsData();
-          // historyItems.innerHTML = html;
-        } else {
-          alert("Something went wrong while adding News!!");
-        }
-      })
-      .catch((err) => {
-        alert("Error: " + err);
-      });
-  });
+    .catch((err) => {
+      alert("Error: " + err);
+    });
+});
 
-  document.forms["events-add"].addEventListener("submit", (e) => {
-    e.preventDefault();
-    fetch(`/api/events/eventsUpload`, {
-      method: "POST",
-      headers: {
-        auth_token: localStorage.getItem("adminToken"),
-      },
-      body: new URLSearchParams(new FormData(e.target)),
+document.forms["events-add"].addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(`/api/events/eventsUpload`, {
+    method: "POST",
+    headers: {
+      auth_token: localStorage.getItem("adminToken"),
+    },
+    body: new URLSearchParams(new FormData(e.target)),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.status == 0) {
+        alert("Events Added Successfully!!");
+        window.location.reload();
+        // fetchNewsData();
+        // fetchEventsData();
+        // historyItems.innerHTML = html;
+      } else {
+        alert("Something went wrong while adding Events!!");
+      }
     })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.status == 0) {
-          alert("Events Added Successfully!!");
-          window.location.reload();
-          // fetchNewsData();
-          // fetchEventsData();
-          // historyItems.innerHTML = html;
-        } else {
-          alert("Something went wrong while adding Events!!");
-        }
-      })
-      .catch((err) => {
-        alert("Error: " + err);
-      });
-  });
+    .catch((err) => {
+      alert("Error: " + err);
+    });
+});
 
-  document.forms["image-add"].addEventListener("submit", (e) => {
-    e.preventDefault();
-    fetch(`/api/admin/carouselUpload`, {
-      method: "POST",
-      headers: {
-        auth_token: `${localStorage.getItem("adminToken")}`,
-      },
-      body: new FormData(e.target),
+document.forms["image-add"].addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(`/api/admin/carouselUpload`, {
+    method: "POST",
+    headers: {
+      auth_token: `${localStorage.getItem("adminToken")}`,
+    },
+    body: new FormData(e.target),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.status == 0) {
+        alert("Images Uploaded Successfully");
+        window.location.reload();
+      } else alert("Something Went Wrong!!");
     })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.status == 0) {
-          alert("Images Uploaded Successfully");
-          window.location.reload();
-        } else alert("Something Went Wrong!!");
-      })
-      .catch((err) => {
-        alert(err);
-      });
-  });
+    .catch((err) => {
+      alert(err);
+    });
+});
 
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
-  const displayNews = (data) => {
-    var html = ``;
-    for (var i = data.length - 1; i >= 0; i--) {
-      html += `<div class="h-item">
+const displayNews = (data) => {
+  var html = ``;
+  for (var i = data.length - 1; i >= 0; i--) {
+    html += `<div class="h-item">
         <div class="time">
             <h3>${data[i].date}</h3>
             <p>${months[data[i].month]}</p>
         </div>
         <div class="h-text">
             <a href="${data[i].news.link}" target="_blank">${
-        data[i].news.desc
-      }</a>
+      data[i].news.desc
+    }</a>
         </div>
         <div class="delete-btn">
             <button class="add-button" onclick="deleteNews('${
@@ -148,42 +148,42 @@ if (window.innerWidth < 992) {
             }');">Delete</button>
         </div>
     </div>`;
-    }
-    // console.log(html);
-    newsHistory.innerHTML = html;
-  };
-
-  function deleteNews(id) {
-    console.log(id);
-    fetch(`/api/news/deleteNews`, {
-      method: "DELETE",
-      body: new URLSearchParams({ id: id }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.status == 0) {
-          alert("News Deleted Successfully");
-          window.location.reload();
-        } else alert("Something Went Wrong!!");
-      })
-      .catch((err) => {
-        alert(err);
-      });
   }
+  // console.log(html);
+  newsHistory.innerHTML = html;
+};
 
-  const displayEvents = (data) => {
-    // console.log(data)
-    var html = ``;
-    for (var i = data.length - 1; i >= 0; i--) {
-      html += `<div class="h-item">
+function deleteNews(id) {
+  console.log(id);
+  fetch(`/api/news/deleteNews`, {
+    method: "DELETE",
+    body: new URLSearchParams({ id: id }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.status == 0) {
+        alert("News Deleted Successfully");
+        window.location.reload();
+      } else alert("Something Went Wrong!!");
+    })
+    .catch((err) => {
+      alert(err);
+    });
+}
+
+const displayEvents = (data) => {
+  // console.log(data)
+  var html = ``;
+  for (var i = data.length - 1; i >= 0; i--) {
+    html += `<div class="h-item">
         <div class="time">
             <h3>${data[i].date}</h3>
             <p>${months[data[i].month]}</p>
         </div>
         <div class="h-text">
             <a href="${data[i].events.link}" target="_blank">${
-        data[i].events.desc
-      }</a>
+      data[i].events.desc
+    }</a>
         </div>
         <div class="delete-btn">
             <button class="add-button" onclick="deleteEvent('${data[
@@ -191,42 +191,42 @@ if (window.innerWidth < 992) {
             ]._id.toString()}');">Delete</button>
         </div>
     </div>`;
-    }
-    // console.log(html);
-    eventsHistory.innerHTML = html;
-  };
-
-  function deleteEvent(id) {
-    // console.log(id);
-    fetch(`/api/events/deleteEvents`, {
-      method: "DELETE",
-      body: new URLSearchParams({ id: id }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.status == 0) {
-          alert("Event Deleted Successfully");
-          window.location.reload();
-        } else alert("Something Went Wrong!!");
-      })
-      .catch((err) => {
-        alert(err);
-      });
   }
+  // console.log(html);
+  eventsHistory.innerHTML = html;
+};
 
-  const showArray = (data) => {
-    let html = ``;
-    data.map((e) => {
-      html += `${e}, `;
+function deleteEvent(id) {
+  // console.log(id);
+  fetch(`/api/events/deleteEvents`, {
+    method: "DELETE",
+    body: new URLSearchParams({ id: id }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.status == 0) {
+        alert("Event Deleted Successfully");
+        window.location.reload();
+      } else alert("Something Went Wrong!!");
+    })
+    .catch((err) => {
+      alert(err);
     });
-    return html.slice(0, -2);
-  };
+}
 
-  const displayRecruiterData = (data) => {
-    // console.log(data[0]._id);
-    let html = ``;
-    for (let i = 0; i < data.length; i++) {
-      html += `<div class="row">
+const showArray = (data) => {
+  let html = ``;
+  data.map((e) => {
+    html += `${e}, `;
+  });
+  return html.slice(0, -2);
+};
+
+const displayRecruiterData = (data) => {
+  // console.log(data[0]._id);
+  let html = ``;
+  for (let i = 0; i < data.length; i++) {
+    html += `<div class="row">
                 <div class="col">
                   <span>${i + 1}</span>
                 </div>
@@ -254,12 +254,12 @@ if (window.innerWidth < 992) {
                   ]._id.toString()}')">View</button>
                 </div>
               </div>`;
-    }
-    detailsShow.innerHTML = html;
-  };
+  }
+  detailsShow.innerHTML = html;
+};
 
-  const displayParticularRecruiter = (data) => {
-    let html = `
+const displayParticularRecruiter = (data) => {
+  let html = `
   <div class="fields-head">
   <h3 style="color:#e7a849">Date of Application: ${data.dof}</h3>
 </div>
@@ -437,139 +437,138 @@ if (window.innerWidth < 992) {
   <span>${data.otherRequirements}</span>
 </div>
   `;
-    recruiterDetails.innerHTML = html;
-  };
+  recruiterDetails.innerHTML = html;
+};
 
-  const displayData = (data) => {
-    document.title = `Welcome !! ${data.name} | ${data.post}`;
-  };
+const displayData = (data) => {
+  document.title = `Welcome !! ${data.name} | ${data.post}`;
+};
 
-  const fetchAdminData = () => {
-    fetch(`/api/admin/adminShow`, {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-        auth_token: `${localStorage.getItem("adminToken")}`,
-      },
+const fetchAdminData = () => {
+  fetch(`/api/admin/adminShow`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      auth_token: `${localStorage.getItem("adminToken")}`,
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.status == 0) {
+        displayData(data.data);
+      } else {
+        localStorage.removeItem("adminToken");
+        window.location.href = "/admin";
+        alert("Status not 0");
+      }
     })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.status == 0) {
-          displayData(data.data);
-        } else {
-          localStorage.removeItem("adminToken");
-          window.location.href = "/admin";
-          alert("Status not 0");
-        }
-      })
-      .catch((err) => {
+    .catch((err) => {
+      // localStorage.removeItem("adminToken");
+      // window.location.href = "./admin-login.html";
+      alert(err);
+    });
+};
+
+const fetchNewsData = () => {
+  fetch(`/api/news/newsFetch`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      auth_token: localStorage.getItem("adminToken"),
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.status == 0) {
+        displayNews(data.data);
+      } else {
         // localStorage.removeItem("adminToken");
         // window.location.href = "./admin-login.html";
-        alert(err);
-      });
-  };
-
-  const fetchNewsData = () => {
-    fetch(`/api/news/newsFetch`, {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-        auth_token: localStorage.getItem("adminToken"),
-      },
+      }
     })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.status == 0) {
-          displayNews(data.data);
-        } else {
-          // localStorage.removeItem("adminToken");
-          // window.location.href = "./admin-login.html";
-        }
-      })
-      .catch((err) => {
+    .catch((err) => {
+      // localStorage.removeItem("adminToken");
+      // window.location.href = "./admin-login.html";
+    });
+};
+
+const fetchEventsData = () => {
+  fetch(`/api/events/eventsFetch`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      auth_token: localStorage.getItem("adminToken"),
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.status == 0) {
+        displayEvents(data.data);
+      } else {
         // localStorage.removeItem("adminToken");
         // window.location.href = "./admin-login.html";
-      });
-  };
-
-  const fetchEventsData = () => {
-    fetch(`/api/events/eventsFetch`, {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-        auth_token: localStorage.getItem("adminToken"),
-      },
+      }
     })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.status == 0) {
-          displayEvents(data.data);
-        } else {
-          // localStorage.removeItem("adminToken");
-          // window.location.href = "./admin-login.html";
-        }
-      })
-      .catch((err) => {
-        // localStorage.removeItem("adminToken");
-        // window.location.href = "./admin-login.html";
-      });
-  };
+    .catch((err) => {
+      // localStorage.removeItem("adminToken");
+      // window.location.href = "./admin-login.html";
+    });
+};
 
-  const fetchRecruitersData = () => {
-    fetch(`/api/admin/jafView`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
+const fetchRecruitersData = () => {
+  fetch(`/api/admin/jafView`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.status == 0) {
+        displayRecruiterData(data.data);
+      } else {
+        alert(data.error);
+      }
     })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.status == 0) {
-          displayRecruiterData(data.data);
-        } else {
-          alert(data.error);
-        }
-      })
-      .catch((err) => {
-        alert(err.message);
-      });
-  };
+    .catch((err) => {
+      alert(err.message);
+    });
+};
 
-  const fetchParticularRecruiter = (id) => {
-    fetch(`/api/admin/jafDetails?id=${id}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.status == 0) {
-          displayParticularRecruiter(data.data);
-        } else {
-          alert(data.error);
-        }
-      })
-      .catch((err) => {
-        alert(err.message);
-      });
-  };
-
-  if (localStorage.getItem("adminToken")) {
-    fetchAdminData();
-    fetchNewsData();
-    fetchEventsData();
-    fetchRecruitersData();
-    // displayDetails();
-  } else {
-    window.location.href = "/admin";
-  }
-
-  backBtn.addEventListener("click", () => {
-    localStorage.removeItem("adminToken");
-    window.location.href = "/admin";
-  });
+if (localStorage.getItem("adminToken")) {
+  fetchAdminData();
+  fetchNewsData();
+  fetchEventsData();
+  fetchRecruitersData();
+  // displayDetails();
+} else {
+  window.location.href = "/admin";
 }
+
+backBtn.addEventListener("click", () => {
+  localStorage.removeItem("adminToken");
+  window.location.href = "/admin";
+});
+
+const fetchParticularRecruiter = (id) => {
+  fetch(`/api/admin/jafDetails?id=${id}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.status == 0) {
+        displayParticularRecruiter(data.data);
+      } else {
+        alert(data.error);
+      }
+    })
+    .catch((err) => {
+      alert(err.message);
+    });
+};
 
 const showDetails = (id) => {
   let overlay = document.querySelector(".overlay");
--- public/js/for-recriters.js
+++ public/js/for-recriters.js
@@ -115,51 +115,51 @@ representatives1.addEventListener("click", () => {
   mainContainerMb[3].style.display = "block";
 });
 
-google.charts.load("current", { packages: ["corechart"] });
-google.charts.setOnLoadCallback(drawChart);
+// google.charts.load("current", { packages: ["corechart"] });
+// google.charts.setOnLoadCallback(drawChart);
 
-function drawChart() {
-  var data1 = google.visualization.arrayToDataTable([
-    ["Branch", "Strength"],
-    ["CSE", 35],
-    ["ECE", 24],
-    ["MeA", 9],
-  ]);
-  var data2 = google.visualization.arrayToDataTable([
-    ["Branch", "Strength"],
-    ["CSE", 52],
-    ["ECE", 48],
-    ["MeA", 18],
-  ]);
+// function drawChart() {
+//   var data1 = google.visualization.arrayToDataTable([
+//     ["Branch", "Strength"],
+//     ["CSE", 35],
+//     ["ECE", 24],
+//     ["MeA", 9],
+//   ]);
+//   var data2 = google.visualization.arrayToDataTable([
+//     ["Branch", "Strength"],
+//     ["CSE", 52],
+//     ["ECE", 48],
+//     ["MeA", 18],
+//   ]);
 
-  var options1 = {
-    title: "2019-2023 Batch (Branchwise Data)",
-    is3D: true,
-    width: 640,
-    height: 380
-  };
-  var options2 = {
-    title: "2020-2024 Batch (Branchwise Data)",
-    is3D: true,
-    width: 640,
-    height: 380
-  };
+//   var options1 = {
+//     title: "2019-2023 Batch (Branchwise Data)",
+//     is3D: true,
+//     width: 640,
+//     height: 380
+//   };
+//   var options2 = {
+//     title: "2020-2024 Batch (Branchwise Data)",
+//     is3D: true,
+//     width: 640,
+//     height: 380
+//   };
 
-  var chart1 = new google.visualization.PieChart(
-    document.getElementById("final1")
-  );
-  var chart2 = new google.visualization.PieChart(
-    document.getElementById("pre-final1")
-  );
-  var chartMob1 = new google.visualization.PieChart(
-    document.getElementById("final-mob1")
-  );
-  var chartMob2 = new google.visualization.PieChart(
-    document.getElementById("pr-final-mob1")
-  );
+//   var chart1 = new google.visualization.PieChart(
+//     document.getElementById("final1")
+//   );
+//   var chart2 = new google.visualization.PieChart(
+//     document.getElementById("pre-final1")
+//   );
+//   var chartMob1 = new google.visualization.PieChart(
+//     document.getElementById("final-mob1")
+//   );
+//   var chartMob2 = new google.visualization.PieChart(
+//     document.getElementById("pr-final-mob1")
+//   );
 
-  chart1.draw(data1, options1);
-  chart2.draw(data2, options2);
-  chartMob1.draw(data1, options1);
-  chartMob2.draw(data2, options2);
-}
+//   chart1.draw(data1, options1);
+//   chart2.draw(data2, options2);
+//   chartMob1.draw(data1, options1);
+//   chartMob2.draw(data2, options2);
+// }
